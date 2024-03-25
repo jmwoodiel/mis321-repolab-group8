a284580f34390a2ec91e79926f5d6555b0cd1f56
Maren O'Hara <maren_ohara@Marens-MacBook-Pro.local>
2024-03-25 10:47:00 -0500

    please for the love of God work

--- Program.cs
+++ Program.cs
@@ -4,3 +4,4 @@ Console.WriteLine("Hello, World! It's Jacob!");
 // Mina's Push
 Console.WriteLine("Hello World! It's Mina:)");
 System.Console.WriteLine("Hello, World! It's Rowe!");
+System.Console.WriteLine("Hello, World! It's Maren!");
