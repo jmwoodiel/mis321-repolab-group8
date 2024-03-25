942fe132166389814ac503052fd46e0219384f85
Maren O'Hara <maren_ohara@Marens-MacBook-Pro.local>
2024-03-25 10:33:27 -0500

    first push

--- Program.cs
+++ Program.cs
@@ -3,3 +3,6 @@ Console.WriteLine("Hello, World! It's Jacob!");
 
 // Mina's Push
 Console.WriteLine("Hello World! It's Mina:)");
+
+// Maren's Push
+System.Console.WriteLine("Hello World! It's Maren!");
