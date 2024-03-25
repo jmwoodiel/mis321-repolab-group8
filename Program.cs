5043b0690b37200a4824ab8ac68a21465fb5f6f6
unknown <jmwoodiel@crimson.ua.edu>
2024-03-25 10:43:44 -0500

    firuing this out?

--- Program.cs
+++ Program.cs
@@ -1,9 +1,6 @@
 ﻿// Jacob's Push
 Console.WriteLine("Hello, World! It's Jacob!");
 
-<<<<<<< HEAD
 // Mina's Push
 Console.WriteLine("Hello World! It's Mina:)");
-=======
 System.Console.WriteLine("Hello, World! It's Rowe!");
->>>>>>> c60659295b47d8dbc9e639d51b33862c0c81292d
